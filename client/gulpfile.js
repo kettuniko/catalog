c3d270bb57bc6c815b0f00ccd61aa5f65384170b
Niko <nk.kopio@gmail.com>
2014-03-03 23:27:12 +0200

    Update gulpfile for angular

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -8,36 +8,44 @@ var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var jade = require('gulp-jade');
 var livereload = require('gulp-livereload');
+var bowerSrc = require('gulp-bower-src');
 var server = livereload();
 
 // Lint Task
 gulp.task('lint', function () {
-  return gulp.src('js/*.js')
+  return gulp.src('app/js/*.js')
       .pipe(jshint())
       .pipe(jshint.reporter('default'))
 });
 
 // Compile Our Sass
 gulp.task('sass', function () {
-  return gulp.src('scss/*.scss')
+  return gulp.src('app/scss/*.scss')
       .pipe(sass())
-      .pipe(gulp.dest('css'))
+      .pipe(gulp.dest('./dist/css'))
 });
 
 // Concatenate & Minify JS
 gulp.task('scripts', function () {
-  return gulp.src('js/*.js')
+  return gulp.src('app/js/*.js')
       .pipe(concat('all.js'))
       .pipe(gulp.dest('dist'))
       .pipe(rename('all.min.js'))
       .pipe(uglify())
       .pipe(gulp.dest('dist'))
+      .pipe(livereload())
 });
-// Jade
+ // Vendor scripts
+gulp.task('vendor', function () {
+  return gulp.src('app/bower_components/**/*.min.js')
+      .pipe(concat('vendor.min.js'))
+      .pipe(gulp.dest('dist/vendor'))
+});
+// Jade main
 gulp.task('templates', function () {
   var YOUR_LOCALS = {};
 
-  gulp.src('*.jade')
+  gulp.src('app/*.jade')
       .pipe(jade({
         locals: YOUR_LOCALS
       }))
@@ -45,12 +53,26 @@ gulp.task('templates', function () {
       .pipe(livereload())
 });
 
+// Jade partials
+gulp.task('partials', function () {
+  var YOUR_LOCALS = {};
+
+  gulp.src('app/partials/*.jade')
+      .pipe(jade({
+        locals: YOUR_LOCALS
+      }))
+      .pipe(gulp.dest('./dist/partials'))
+      .pipe(livereload())
+});
+
+
 // Watch Files For Changes
 gulp.task('watch', function () {
-  gulp.watch('js/*.js', ['lint', 'scripts']);
-  gulp.watch('scss/*.scss', ['sass']);
-  gulp.watch('*.jade', ['templates']);
+  gulp.watch('app/js/*.js', ['lint', 'scripts']);
+  gulp.watch('app/scss/*.scss', ['sass']);
+  gulp.watch('app/*.jade', ['templates']);
+  gulp.watch('app/partials/*.jade', ['partials']);
 });
 
 // Default Task
-gulp.task('default', ['lint', 'sass', 'scripts', 'watch', 'templates']);
+gulp.task('default', ['lint', 'sass', 'vendor', 'scripts', 'watch', 'templates', 'partials']);
