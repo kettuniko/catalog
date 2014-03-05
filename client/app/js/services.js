9bd3ebe8e8f32ab962d0f14c02dfc6c5dece2676
Niko <nk.kopio@gmail.com>
2014-03-05 23:07:12 +0200

    Use server constant in Items-service

--- client/app/js/services.js
+++ client/app/js/services.js
@@ -3,12 +3,14 @@
 /* Services */
 var SERVER_URL = 'http://localhost:8081';
 
-angular.module('myApp.services', ['ngResource']).factory('Items', function ($resource) {
-    return $resource('http://localhost:8081/items', {}, {
+angular.module('myApp.services', ['ngResource'])
+    .factory('Items', function ($resource) {
+      return $resource(SERVER_URL + '/items', {}, {
         query: {method: 'GET', isArray: true}
+      });
+    })
+    .factory('Import', function ($resource) {
+      return $resource(SERVER_URL + '/import/steam', {}, {
+        query: {method: 'GET', isArray: true}
+      });
     });
-}).factory('Import', function ($resource) {
-    return $resource(SERVER_URL + '/import/steam', {}, {
-        query: {method: 'GET', isArray: true}
-  });
-});
